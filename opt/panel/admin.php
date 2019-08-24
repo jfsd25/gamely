ce9fb419ef2d1fc18a7d43f349c6813d063d71ff
jfsd25 <54447428+jfsd25@users.noreply.github.com>
2019-08-25 00:17:59 +0800

    Update admin.php

--- opt/panel/admin.php
+++ opt/panel/admin.php
@@ -279,7 +279,7 @@ if(isset($_POST['action'])) {
 							<option value="1.8.8">Spigot 1.8.8</option>
 							<option value="1.7.10">Spigot 1.7.10</option>
 							<option value="BC">Bungeecord</option>
-							<option value="NONE">None</option>
+							<option value="1.14.4">Spigot 1.14.4</option>
 						</select>
 					</div>
 				</div>
